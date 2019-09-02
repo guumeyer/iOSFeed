4a12be9a6aff1eb0f8e49f8f356bc78085c518c5
Gustavo R. Meyer <guumeyer@gmail.com>
2019-09-01 22:50:39 -0700

    Initial Commit

---
+++ Feed/Feed.h
@@ -0,0 +1,19 @@
+//
+//  Feed.h
+//  Feed
+//
+//  Created by Gustavo on 2019-09-01.
+//  Copyright © 2019 Gustavo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Feed.
+FOUNDATION_EXPORT double FeedVersionNumber;
+
+//! Project version string for Feed.
+FOUNDATION_EXPORT const unsigned char FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feed/PublicHeader.h>
+
+
